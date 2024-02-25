462920d559f7bc956d1f5e3a5f4cf5f93a78e2fa
Delgersaikhannn <107856127+Delgersaikhannn@users.noreply.github.com>
2024-02-25 20:28:10 +0800

    Update contract.sol
    Added :
    - mint price 0.15 eth
    - mint price update
    - receive collected amount only owner
    - safemint restrictions

--- contract.sol
+++ contract.sol
@@ -4,18 +4,47 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract MyToken is ERC721, ERC721Burnable, Ownable {
+    using SafeMath for uint256;
+
     event TokenMinted(address indexed owner, uint256 indexed tokenId);
 
-    constructor() ERC721("TENSION", "TSN") {}
+    uint256 public mintPrice = 0.15 ether;
+    string tokenName = "TENSION";
+    string tokenSymbol = "TSN";
+    string baseURI =
+        "https://bafybeihrutv46c2qcd4v63lu6vqzpu66kzulivtl7umorlwb53voost324.ipfs.nftstorage.link/";
+
+    constructor() ERC721(tokenName, tokenSymbol) Ownable(msg.sender) {}
 
     function _baseURI() internal view virtual override returns (string memory) {
-        return "https://bafybeihrutv46c2qcd4v63lu6vqzpu66kzulivtl7umorlwb53voost324.ipfs.nftstorage.link/";
+        return baseURI;
     }
 
-    function safeMint(address to, uint256 tokenId) public {
+    function safeMint(address to, uint256 tokenId) public payable {
+        require(msg.value == mintPrice, "Incorrect Ether amount sent");
+        require(
+            ownerOf(tokenId) == address(0),
+            "Token with this ID already exists"
+        );
+
         _safeMint(to, tokenId);
         emit TokenMinted(to, tokenId);
+    }
+
+    function setMintPrice(uint256 newPrice) external onlyOwner {
+        mintPrice = newPrice;
+    }
+
+    receive() external payable onlyOwner {
+        address payable ownerAddress = payable(owner());
+        ownerAddress.transfer(msg.value);
+    }
+
+    function transferOwnership(address newOwner) public override onlyOwner {
+        require(newOwner != address(0), "New owner cannot be the zero address");
+        _transferOwnership(newOwner);
     }
 }
